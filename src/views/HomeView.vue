65cd898caac1415f01ba5d74550f53d88a048e36
alireza nezami <nezamialireza1022@gmail.com>
2025-02-09 21:04:42 +0330

    f

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,9 @@
+<script setup>
+import { BeakerIcon } from '@heroicons/vue/24/solid'
+</script>
+
 <template>
-  <div class="flex flex-col justify-center items-center">
+  <div class="flex flex-col justify-center items-center gap-6">
     <!-- /// profile  -->
     <div class="w-full flex justify-start items-center gap-4">
       <div>
@@ -9,11 +13,16 @@
         <p class="text-gray600 flex justify-center items-center text-[16px] leading-[140%] gap-1">
           صبح بخیر
           <span>
-            <img src="../assets/images/image.png" class="w-[18px]" />
+            <img src="../assets/images/image.png" class="w-[20px]" />
           </span>
         </p>
         <h5 class="font-bold text-[20px] leading-[120%]">علیرضا نظامی</h5>
       </div>
+    </div>
+
+    <!-- /// search  -->
+    <div>
+      <BeakerIcon />
     </div>
   </div>
 </template>
