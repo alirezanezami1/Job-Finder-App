9ccdc9e6e864bbe6ecb4df13cf39be3405788c3e
alireza nezami <nezamialireza1022@gmail.com>
2025-02-10 23:07:24 +0330

    completing banner section

--- src/components/BannerHome.vue
+++ src/components/BannerHome.vue
@@ -1,3 +1,17 @@
 <template>
-  <div></div>
+  <div class="w-[410px] flex justify-center items-center relative">
+    <img src="../assets/images/Tips & Walkthrough.png" alt="banner" class="object-cover w-fit" />
+    <div
+      class="flex flex-col justify-center items-center gap-4 absolute w-[166px] left-9 bottom-[66px]"
+    >
+      <h5 class="font-bold text-[20px] leading-[140%] text-center text-white">
+        سه سوته کار پیدا کن !
+      </h5>
+      <button
+        class="text-primary500 bg-white text-[14px] leading-[140%] px-4 py-[6px] rounded-full"
+      >
+        مشاهده بیشتر
+      </button>
+    </div>
+  </div>
 </template>
