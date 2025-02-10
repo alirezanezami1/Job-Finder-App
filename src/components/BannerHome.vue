642dd928f9864b449dd1dab7be626852cdf02302
alireza nezami <nezamialireza1022@gmail.com>
2025-02-10 23:50:55 +0330

    edit feature in banner section

--- src/components/BannerHome.vue
+++ src/components/BannerHome.vue
@@ -1,9 +1,9 @@
 <template>
-  <div class="w-[410px] flex justify-center items-center relative">
+  <div
+    class="flex justify-center items-center relative rounded-full shadow-[4px_12px_32px_rgba(36,107,253,0.2)]"
+  >
     <img src="../assets/images/Tips & Walkthrough.png" alt="banner" class="object-cover w-fit" />
-    <div
-      class="flex flex-col justify-center items-center gap-4 absolute w-[166px] left-9 bottom-[66px]"
-    >
+    <div class="flex flex-col justify-center items-center gap-4 absolute w-[166px] left-4 bottom-8">
       <h5 class="font-bold text-[20px] leading-[140%] text-center text-white">
         سه سوته کار پیدا کن !
       </h5>
