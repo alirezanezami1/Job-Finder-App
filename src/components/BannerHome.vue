0e3c7b9acdb3777f3a4c4a5ae17ecbaf6c030dab
alireza nezami <nezamialireza1022@gmail.com>
2025-02-19 00:08:08 +0330

    add feature

--- src/components/BannerHome.vue
+++ src/components/BannerHome.vue
@@ -2,7 +2,7 @@
   <div
     class="flex justify-center items-center relative rounded-full shadow-[4px_12px_32px_rgba(36,107,253,0.2)]"
   >
-    <img src="../assets/images/Tips & Walkthrough.png" alt="banner" class="object-cover w-fit" />
+    <img src="../assets/images/Tips & Walkthrough.png" alt="banner" class="object-cover w-full" />
     <div class="flex flex-col justify-center items-center gap-4 absolute w-[166px] left-4 bottom-8">
       <h5 class="font-bold text-[20px] leading-[140%] text-center text-white">
         سه سوته کار پیدا کن !
--- src/components/TemplateNavbar.vue
+++ src/components/TemplateNavbar.vue
@@ -1,5 +1,7 @@
 <template>
-  <div class="flex flex-col gap-[2px] items-center text-gray500 hover:text-primary500">
+  <div
+    class="flex flex-col gap-[2px] cursor-pointer items-center text-gray500 hover:text-primary500"
+  >
     <slot></slot>
   </div>
 </template>
