0c6d75a896032abd3489642a0bfebd194d9dd805
alireza nezami <nezamialireza1022@gmail.com>
2025-02-19 22:49:50 +0330

    the end of the component notification

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="h-screen flex flex-col items-center mx-auto" style="width: 428px">
-    <div class="w-full flex-1 overflow-y-auto px-4">
+    <div class="w-full flex-1 overflow-y-auto px-6">
       <RouterView class="w-full font-vazir bg-white py-4" dir="rtl" />
     </div>
   </div>
--- src/views/NotificationView.vue
+++ src/views/NotificationView.vue
@@ -1,3 +1,23 @@
+<script setup>
+import { ArrowRightIcon } from '@heroicons/vue/24/outline'
+import { RouterLink } from 'vue-router'
+</script>
+
 <template>
-  <div></div>
+  <div class="flex flex-col justify-center items-center pb-16 gap-6">
+    <div class="flex justify-start items-center gap-4 w-full">
+      <RouterLink to="/" class="w-[28px]">
+        <ArrowRightIcon class="text-gray900" />
+      </RouterLink>
+      <h4 class="text-[24px] leading-[120%] font-bold text-gray900">اعلان ها</h4>
+    </div>
+
+    <div class="flex flex-col justify-center items-center gap-[60px] py-[90px]">
+      <img src="../assets/images/Frame.png" alt="empty" />
+      <div class="flex flex-col gap-3 justify-center items-center text-gray900">
+        <h4 class="text-[24px] leading-[120%] font-bold">خالی!</h4>
+        <p class="text-[18px] leading-[140%] font-medium">شما فعلا اعلانی نداری :(</p>
+      </div>
+    </div>
+  </div>
 </template>
