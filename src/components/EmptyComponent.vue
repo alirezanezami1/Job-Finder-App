0add233f9587a736bc27273ce424a01a3ef2b013
alireza nezami <nezamialireza1022@gmail.com>
2025-03-06 22:07:38 +0330

    new component emptyComponent

---
+++ src/components/EmptyComponent.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { ArrowLeftIcon } from '@heroicons/vue/24/solid'
+</script>
+
+<template>
+  <div class="flex flex-col justify-center items-center pb-16 gap-6">
+    <div class="flex justify-between items-center gap-4 w-full">
+      <RouterLink to="/" class="flex justify-start items-center gap-4">
+        <img src="../assets/Logo.PNG" alt="Logo" class="w-[58px]" />
+      </RouterLink>
+      <RouterLink to="/" class="w-[28px]">
+        <ArrowLeftIcon class="text-gray900" />
+      </RouterLink>
+    </div>
+
+    <div class="flex flex-col justify-center items-center gap-[60px] py-[90px]">
+      <img src="../assets/images/Empty Data.png" alt="empty" />
+      <div class="flex flex-col gap-3 justify-center items-center text-gray900">
+        <h4 class="text-[24px] leading-[120%] font-bold">خالی!</h4>
+        <p class="text-[18px] leading-[140%] font-medium">شما شغلی را ذخیره نکرده اید.</p>
+      </div>
+    </div>
+  </div>
+</template>
--- src/views/BookMarkView.vue
+++ src/views/BookMarkView.vue
@@ -2,10 +2,12 @@
 import NavbarMenu from '@/components/NavbarMenu.vue'
 import JobDetails from '@/components/JobDetails.vue'
 import SkeletonTemplate from '@/components/SkeletonTemplate.vue'
+import EmptyComponent from '@/components/EmptyComponent.vue'
 import { fetchJobs } from '@/api/api.js'
-import { computed, onMounted, ref } from 'vue'
+import { computed, onMounted, ref, watch } from 'vue'
 import { EllipsisHorizontalCircleIcon } from '@heroicons/vue/24/outline'
 
+const showEmpty = ref(false)
 const jobs = ref(null)
 const savedJobs = computed(() => {
   return jobs.value ? jobs.value.filter((job) => job.isSaved) : []
@@ -17,31 +19,45 @@ onMounted(async () => {
   jobs.value = getJobs
   isLoading.value = false
 })
+
+watch(savedJobs, () => {
+  if (!savedJobs.value.length) {
+    showEmpty.value = true
+  } else {
+    showEmpty.value = false
+  }
+})
 </script>
 
 <template>
-  <div v-if="isLoading" class="pt-24">
-    <SkeletonTemplate />
-    <SkeletonTemplate />
-    <SkeletonTemplate />
+  <div v-if="showEmpty">
+    <EmptyComponent />
   </div>
-  <div class="flex flex-col justify-center items-center px-6 py-4 pb-20" v-else>
-    <div class="flex justify-between items-center w-full py-3 gap-3">
-      <div class="flex justify-start items-center gap-4">
-        <img src="../assets/Logo.PNG" alt="Logo" class="w-[58px]" />
+
+  <div v-else>
+    <div v-if="isLoading" class="pt-24">
+      <SkeletonTemplate />
+      <SkeletonTemplate />
+      <SkeletonTemplate />
+    </div>
+    <div class="flex flex-col justify-center items-center px-6 py-4 pb-20" v-else>
+      <div class="flex justify-between items-center w-full py-3 gap-3">
+        <RouterLink to="/" class="flex justify-start items-center gap-4">
+          <img src="../assets/Logo.PNG" alt="Logo" class="w-[58px]" />
+        </RouterLink>
+        <EllipsisHorizontalCircleIcon class="w-[28px]" />
       </div>
-      <EllipsisHorizontalCircleIcon class="w-[28px]" />
-    </div>
 
-    <div
-      v-for="job in savedJobs"
-      :key="job.id"
-      class="p-5 border rounded-[28px] flex flex-col justify-center items-center gap-3 mb-5"
-      v-auto-animate
-    >
-      <JobDetails :jobProp="job" />
-    </div>
+      <div
+        v-for="job in savedJobs"
+        :key="job.id"
+        class="p-5 border rounded-[28px] flex flex-col justify-center items-center gap-3 mb-5"
+        v-auto-animate
+      >
+        <JobDetails :jobProp="job" />
+      </div>
 
-    <NavbarMenu />
+      <NavbarMenu />
+    </div>
   </div>
 </template>
