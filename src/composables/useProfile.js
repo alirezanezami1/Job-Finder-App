579ff61506acb51293b59865132713e8c2d2544e
alireza nezami <nezamialireza1022@gmail.com>
2025-04-24 23:54:18 +0330

    f

--- src/composables/useProfile.js
+++ src/composables/useProfile.js
@@ -1,27 +1,3 @@
 export function () {
-    const user = useUser()
-    const { t } = useI18n()
-
-    const profile = computed(() => {
-        return {
-        name: user.value.name,
-        email: user.value.email,
-        phone: user.value.phone,
-        address: user.value.address,
-        }
-    })
-
-    const updateProfile = async (newProfile) => {
-        try {
-        await user.value.update(newProfile)
-        return { success: true, message: t('profile.updateSuccess') }
-        } catch (error) {
-        return { success: false, message: t('profile.updateError') }
-        }
-    }
-
-    return {
-        profile,
-        updateProfile,
-    }
+    return 'hi'
 }
