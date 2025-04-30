7f024c8caba5e9ffc34bec51972979d771d9d24b
alireza nezami <nezamialireza1022@gmail.com>
2025-04-30 17:40:03 +0330

    change style

--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -29,7 +29,7 @@ import { RouterLink } from 'vue-router'
         <!-- //// name  -->
         <div class="flex flex-col justify-center items-start gap-2 w-[244px]">
           <h4 class="text-[24px] font-bold leading-[120%] text-gray900">علیرضا نظامی</h4>
-          <p class="text-[16px] leading-[140%] font-normal">Ui</p>
+          <p class="text-[16px] leading-[140%] font-normal text-gray700">Ui</p>
         </div>
 
         <div>
