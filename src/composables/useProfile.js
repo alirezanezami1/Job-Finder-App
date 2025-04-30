0fd2a8c609b8e4bca03e6ad01b740c9b230eb209
alireza nezami <nezamialireza1022@gmail.com>
2025-04-30 20:48:23 +0330

    complete the info for composable

--- src/composables/useProfile.js
+++ src/composables/useProfile.js
@@ -1,3 +1,71 @@
-export function () {
-    return 'hi'
+import { ref } from 'vue'
+
+export function useProfile() {
+  const profile = ref({
+    basicInfo: {
+      firstName: 'علیرضا',
+      lastName: 'نظامی',
+      currentPosition: 'Front End developer',
+      summary: '',
+    },
+
+    contactInfo: {
+      email: '',
+      phone: '',
+      address: '',
+    },
+
+    workExperience: [
+      {
+        title: '',
+        company: '',
+        startDate: '',
+        endDate: '',
+        currently: false,
+        description: '',
+        type: '',
+        location: '',
+      },
+    ],
+
+    projects: [
+      {
+        name: '',
+        description: '',
+        startDate: '',
+        endDate: '',
+        currently: false,
+        link: '',
+      },
+    ],
+  })
+
+  const updateProfileSection = (section, data) => {
+    if (Array.isArray(profile.value[section])) {
+      profile.value[section] = [...data]
+    } else {
+      profile.value[section] = { ...profile.value[section], ...data }
+    }
+  }
+
+  // متد برای اضافه کردن آیتم به لیست‌ها (مثل تجربه کاری یا تحصیلات)
+  const addToProfileSection = (section, item) => {
+    if (Array.isArray(profile.value[section])) {
+      profile.value[section].push(item)
+    }
+  }
+
+  // متد برای حذف آیتم از لیست‌ها
+  const removeFromProfileSection = (section, index) => {
+    if (Array.isArray(profile.value[section])) {
+      profile.value[section].splice(index, 1)
+    }
+  }
+
+  return {
+    profile,
+    updateProfileSection,
+    addToProfileSection,
+    removeFromProfileSection,
+  }
 }
