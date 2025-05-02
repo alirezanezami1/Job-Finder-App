d8e15da80a37da117b12bfcf01735c9a2126053a
alireza nezami <nezamialireza1022@gmail.com>
2025-05-02 23:12:48 +0330

    edti-profile

--- src/views/EditProfileView.vue
+++ src/views/EditProfileView.vue
@@ -1,3 +1,10 @@
+<script setup>
+import { ref } from 'vue'
+import { useProfile } from '@/composables/useProfile'
+
+const { profile, updateProfileSection } = useProfile()
+</script>
+
 <template>
   <div></div>
 </template>
