671ac9fd56a376c5d6aed3c241a5d5303d997933
alireza nezami <nezamialireza1022@gmail.com>
2025-05-04 19:14:58 +0330

    generate save btn

---
+++ src/components/SaveBtnView.vue
@@ -0,0 +1,12 @@
+<template>
+  <div
+    class="fixed bottom-0 z-10 w-mobile bg-white border-t border-gray100 p-6 pb-9 text-white cursor-pointer"
+  >
+    <button
+      type="button"
+      class="px-4 py-[18px] rounded-full flex justify-center items-center shadow-btn w-full text-[16px] leading-[140%] font-bold disabled:cursor-not-allowed disabled:opacity-50 disabled:shadow-none bg-primary500 text-white cursor-pointer"
+    >
+      ذخیره
+    </button>
+  </div>
+</template>
