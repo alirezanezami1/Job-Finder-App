80888a018a4f054734c7e41cda0760c4d0af7af0
alireza nezami <nezamialireza1022@gmail.com>
2025-08-10 12:50:39 +0330

    edit composable

--- src/views/EditWorkExpView.vue
+++ src/views/EditWorkExpView.vue
@@ -11,6 +11,17 @@ import { useProfile } from '../composables/useProfile'
 
 const { profile } = useProfile()
 
+const formaData = ref({
+  title: '',
+  company: '',
+  startDate: '',
+  endDate: '',
+  currently: false,
+  description: '',
+  type: '',
+  location: '',
+})
+
 const show = ref(false)
 const show2 = ref(false)
 const isStillWorking = ref(false)
@@ -23,7 +34,7 @@ const goBack = () => {
   router.back()
 }
 
-const searchQuery = ref('')
+const searchQuery = ref(formaData.value.location || '')
 const showDropdown = ref(false)
 
 const { filteredCities } = useCity(searchQuery)
@@ -37,6 +48,17 @@ const getWorkExp = () => {
   const workExp = profile.value.workExperience.find((exp) => exp.id === Number(workExpId))
   if (!workExp) {
     router.push({ name: 'notFound' })
+  } else {
+    formaData.value = {
+      title: workExp.title,
+      company: workExp.company,
+      startDate: workExp.startDate,
+      endDate: workExp.endDate,
+      currently: workExp.currently,
+      description: workExp.description,
+      type: workExp.type,
+      location: workExp.location,
+    }
   }
 }
 
@@ -61,6 +83,7 @@ onMounted(() => {
       <p class="text-[16px] leading-[140%] text-gray800 font-normal">عنوان شغلی</p>
       <input
         type="text"
+        v-model="formaData.title"
         class="flex gap-3 px-5 py-4 rounded-2xl bg-gray50 w-full focus:outline-none placeholder:text-gray500"
         placeholder="عنوان شغلی شما"
       />
@@ -70,6 +93,7 @@ onMounted(() => {
       <p class="text-[16px] leading-[140%] text-gray800 font-normal">سازمان مربوطه</p>
       <input
         type="text"
+        v-model="formaData.company"
         class="flex gap-3 px-5 py-4 rounded-2xl bg-gray50 w-full focus:outline-none placeholder:text-gray500"
         placeholder="سازمان مربوطه شما"
       />
@@ -80,6 +104,7 @@ onMounted(() => {
         <p class="text-[16px] leading-[140%] text-gray800 font-medium">تاریخ شروع</p>
         <input
           type="text"
+          v-model="formaData.startDate"
           class="flex gap-3 p-5 rounded-2xl bg-gray50 w-[160px] h-[56px] focus:outline-none placeholder:text-gray500 relative"
           placeholder="از"
           id="my-custom-editable-input"
@@ -105,16 +130,17 @@ onMounted(() => {
 
         <input
           type="text"
+          v-model="formaData.endDate"
           class="flex gap-3 p-5 rounded-2xl bg-gray50 w-[160px] h-[56px] focus:outline-none placeholder:text-gray500 relative"
-          :class="{ 'opacity-45': isStillWorking }"
+          :class="{ 'opacity-45': formaData.currently }"
           placeholder="تا"
           id="my-custom-editable-input2"
           disabled
         />
         <ChevronDownIcon
-          @click="!isStillWorking && (show2 = true)"
+          @click="!formaData.currently && (show2 = true)"
           class="cursor-pointer absolute w-[17px] left-3 top-[52%]"
-          :class="{ 'opacity-50 cursor-not-allowed': isStillWorking }"
+          :class="{ 'opacity-50 cursor-not-allowed': formaData.currently }"
         />
         <date-picker
           :show="show2"
@@ -125,7 +151,7 @@ onMounted(() => {
           type="year-month"
           simple
           @close="show2 = false"
-          :disabled="isStillWorking"
+          :disabled="formaData.currently"
         />
       </div>
     </div>
@@ -133,7 +159,7 @@ onMounted(() => {
     <div class="w-full flex justify-start items-center gap-5">
       <p class="text-[16px] leading-[140%] text-gray900 font-medium">هنوز مشغول به کارم</p>
       <label class="relative flex items-center cursor-pointer">
-        <input type="checkbox" v-model="isStillWorking" class="sr-only peer" />
+        <input type="checkbox" v-model="formaData.currently" class="sr-only peer" />
         <div
           class="w-9 h-5 bg-blue-200 hover:bg-blue-300 peer-focus:outline-0 rounded-full peer transition-all ease-in-out duration-500 peer-checked:after:translate-x-full peer-checked:after:border-transparent after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-blue-300 after:border after:rounded-full after:h-4 after:w-4 after:transition-all peer-checked:bg-blue-500 hover:peer-checked:bg-blue-600"
         ></div>
@@ -144,7 +170,7 @@ onMounted(() => {
       <p class="text-[16px] leading-[140%] text-gray800 font-normal">توضیحات ( اختیاری )</p>
       <textarea
         type="text"
-        v-model="summary"
+        v-model="formaData.description"
         class="flex gap-3 p-5 h-[250px] resize-none rounded-2xl bg-gray50 w-full focus:outline-none placeholder:text-gray500"
         placeholder="توضیحات شما"
       />
@@ -155,6 +181,7 @@ onMounted(() => {
       <div class="relative w-full">
         <select
           name="currentPosition"
+          v-model="formaData.type"
           id="positions"
           class="flex px-5 pl-8 py-4 rounded-2xl cursor-pointer bg-gray50 w-full focus:outline-none appearance-none"
         >
@@ -175,16 +202,15 @@ onMounted(() => {
         <p class="text-[16px] leading-[140%] text-gray800 font-normal">آدرس</p>
         <div class="relative w-full">
           <input
-            v-model="searchQuery"
             type="text"
+            v-model="formaData.location"
             class="flex gap-3 px-5 pr-10 py-4 rounded-2xl bg-gray50 w-full focus:outline-none placeholder:text-gray500"
             placeholder="آدرس"
             @focus="showDropdown = true"
-            @input="contactInfo.address = searchQuery"
+            @input="formaData.location = searchQuery"
           />
           <MapPinIcon class="absolute w-[20px] top-1/2 right-3 -translate-y-1/2" />
 
-          <!-- Dropdown for cities -->
           <div
             v-if="showDropdown && filteredCities.length > 0"
             class="absolute top-full left-0 right-0 mt-1 bg-white border rounded-lg shadow-lg max-h-60 overflow-y-auto z-10"
